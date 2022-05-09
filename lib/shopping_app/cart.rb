4e22ba1295a56cfb0a7ec90441c452af0b9c3d2a
Rattuti <i.ishiharar.96296@gmail.com>
2022-05-09 16:14:55 +0900

    second commit

--- lib/shopping_app/cart.rb
+++ lib/shopping_app/cart.rb
@@ -1,7 +1,8 @@
-require_relative "item_manager"
+require_relative "ownable"
 
 class Cart
   include ItemManager
+  include Ownable
 
   def initialize(owner)
     self.owner = owner
@@ -26,8 +27,13 @@ class Cart
     return if owner.wallet.balance < total_amount
   # ## 要件
   #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
+  
+
   #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
+  
+  
   #   - カートの中身（Cart#items）が空になること。
+  
 
   # ## ヒント
   #   - カートのオーナーのウォレット ==> self.owner.wallet
